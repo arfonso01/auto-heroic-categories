d2bdc66f8f5acd224d6d86aef18c069f9e8b2eb0
arfonso01 <arfonso87@hotmail.com>
2024-05-20 23:03:09 +0200

    python is python3

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python heroic_categories.py
-python get_categories_for_game.py
+python3 heroic_categories.py
+python3 get_categories_for_game.py
 deactivate
 exit
